d5b0c927cd015efd17a0bd623e2221b72dbb0ce0
Luis_Lopez98 <183785@ids.upchiapas.edu.mx>
2022-10-04 20:30:50 -0500

    se compuso

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require("express");
 var router=express.Router();
 var bodyParser  = require("body-parser");
-var aplicacion = express();
+var app = express();
 var usuarios=require("./routes/rutasUsuario");
 router.get('/', function(request, response) {  
    response.status(200).json({"mensaje":"Sacala la cawuama nodejs"});
 });
-aplicacion.use(bodyParser.json()); 
+app.use(bodyParser.json()); 
 //incluimos el archivo en el que se almacenan las rutas de cada entidad
-aplicacion.use(router);  
-aplicacion.use(usuarios);
+app.use(router);  
+app.use(usuarios);
  
 
-aplicacion.listen(5000, function() { 
+app.listen(5000, function() { 
 console.log("Servidor iniciado");
 });
